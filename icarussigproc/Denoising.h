07f4f8830b09c6486327ceadafdaadd9d2d2a643
Tracy Usher <usher@slac.stanford.edu>
2020-02-03 15:45:55 -0800

    Formatting

--- icarussigproc/Denoising.h
+++ icarussigproc/Denoising.h
@@ -28,184 +28,177 @@
 
 namespace icarussigproc {
 
-  /**
-     \class Denoising
-     User defined class Denoising ... these comments are used to generate
-     doxygen documentation!
-  */
-  class Denoising{
-    
-    public:
-      
-      /// Default constructor
-      Denoising(){}
+/**
+   \class Denoising
+   User defined class Denoising ... these comments are used to generate
+   doxygen documentation!
+*/
+class Denoising {
+  
+public:
+  
+  /// Default constructor
+  Denoising(){}
 
-      void getSelectVals(
-        const ArrayShort&,
-        const ArrayShort&,
-        ArrayBool&,
-        ArrayBool&,
-        const unsigned int,
-        const float);
+  void getSelectVals(const ArrayShort&,
+                     const ArrayShort&,
+                     ArrayBool&,
+                     ArrayBool&,
+                     const unsigned int,
+                     const float);
 
-      void getSelectVals(
-        const ArrayFloat&,
-        const ArrayFloat&,
-        ArrayBool&,
-        ArrayBool&,
-        const unsigned int,
-        const float);
+  void getSelectVals(
+    const ArrayFloat&,
+    const ArrayFloat&,
+    ArrayBool&,
+    ArrayBool&,
+    const unsigned int,
+    const float);
 
-      void getSelectVals(
-        const ArrayDouble&,
-        const ArrayDouble&,
-        ArrayBool&,
-        ArrayBool&,
-        const unsigned int,
-        const float);
+  void getSelectVals(
+    const ArrayDouble&,
+    const ArrayDouble&,
+    ArrayBool&,
+    ArrayBool&,
+    const unsigned int,
+    const float);
 
+  void removeCoherentNoise1D(
+    ArrayShort&,
+    const ArrayShort&,
+    ArrayShort&,
+    ArrayShort&,
+    ArrayBool&,
+    ArrayBool&,
+    ArrayShort&,
+    const char,
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const float 
+  );
 
-      void removeCoherentNoise1D(
-        ArrayShort&,
-        const ArrayShort&,
-        ArrayShort&,
-        ArrayShort&,
-        ArrayBool&,
-        ArrayBool&,
-        ArrayShort&,
-        const char,
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const float 
-      );
+  void removeCoherentNoise1D(
+    ArrayFloat&,
+    const ArrayFloat&,
+    ArrayFloat&,
+    ArrayFloat&,
+    ArrayBool&,
+    ArrayBool&,
+    ArrayFloat&,
+    const char,
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const float 
+  );
 
-      void removeCoherentNoise1D(
-        ArrayFloat&,
-        const ArrayFloat&,
-        ArrayFloat&,
-        ArrayFloat&,
-        ArrayBool&,
-        ArrayBool&,
-        ArrayFloat&,
-        const char,
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const float 
-      );
+  void removeCoherentNoise1D(
+    ArrayDouble&, 
+    const ArrayDouble&,
+    ArrayDouble&,
+    ArrayDouble&,
+    ArrayBool&,
+    ArrayBool&,
+    ArrayDouble&,
+    const char,
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const float 
+  );
 
-      void removeCoherentNoise1D(
-        ArrayDouble&, 
-        const ArrayDouble&,
-        ArrayDouble&,
-        ArrayDouble&,
-        ArrayBool&,
-        ArrayBool&,
-        ArrayDouble&,
-        const char,
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const float 
-      );
+  void removeCoherentNoise2D(
+    ArrayShort&,
+    const ArrayShort&, 
+    ArrayShort&,
+    ArrayShort&,
+    ArrayBool&,
+    ArrayBool&,
+    ArrayShort&,
+    const char, 
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const float);
 
+  void removeCoherentNoise2D(
+    ArrayFloat&,
+    const ArrayFloat&, 
+    ArrayFloat&,
+    ArrayFloat&,
+    ArrayBool&,
+    ArrayBool&,
+    ArrayFloat&,
+    const char, 
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const float);
 
-      void removeCoherentNoise2D(
-        ArrayShort&,
-        const ArrayShort&, 
-        ArrayShort&,
-        ArrayShort&,
-        ArrayBool&,
-        ArrayBool&,
-        ArrayShort&,
-        const char, 
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const float);
+  void removeCoherentNoise2D(
+    ArrayDouble&, 
+    const ArrayDouble&, 
+    ArrayDouble&,
+    ArrayDouble&,
+    ArrayBool&,
+    ArrayBool&,
+    ArrayDouble&,
+    const char, 
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const unsigned int,
+    const float);
 
-      void removeCoherentNoise2D(
-        ArrayFloat&,
-        const ArrayFloat&, 
-        ArrayFloat&,
-        ArrayFloat&,
-        ArrayBool&,
-        ArrayBool&,
-        ArrayFloat&,
-        const char, 
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const float);
+/// Default destructor
+~Denoising(){}
 
-      void removeCoherentNoise2D(
-        ArrayDouble&, 
-        const ArrayDouble&, 
-        ArrayDouble&,
-        ArrayDouble&,
-        ArrayBool&,
-        ArrayBool&,
-        ArrayDouble&,
-        const char, 
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const unsigned int,
-        const float);
+private:
+  template <typename T>
+  void getSelectVals(
+    const std::vector<std::vector<T> >& waveforms,
+    const std::vector<std::vector<T> >& morphedWaveforms,
+    ArrayBool& selectVals,
+    ArrayBool& roi,
+    const unsigned int window,
+    const float thresholdFactor
+  );
 
-    
-    /// Default destructor
-    ~Denoising(){}
+  template <typename T>
+  void removeCoherentNoise1D(
+    std::vector<std::vector<T> >& waveLessCoherent, 
+    const std::vector<std::vector<T> >& filteredWaveforms, 
+    std::vector<std::vector<T> >& morphedWaveforms, 
+    std::vector<std::vector<T> >& intrinsicRMS,
+    ArrayBool& selectVals,
+    ArrayBool& roi,
+    std::vector<std::vector<T> >& correctedMedians,
+    const char filterName='d', 
+    const unsigned int grouping=64,
+    const unsigned int structuringElement=5,
+    const unsigned int window=0,
+    const float thresholdFactor=2.5);
 
-    private:
-
-      template <typename T>
-      void getSelectVals(
-        const std::vector<std::vector<T> >& waveforms,
-        const std::vector<std::vector<T> >& morphedWaveforms,
-        ArrayBool& selectVals,
-        ArrayBool& roi,
-        const unsigned int window,
-        const float thresholdFactor
-      );
-
-
-      template <typename T>
-      void removeCoherentNoise1D(
-        std::vector<std::vector<T> >& waveLessCoherent, 
-        const std::vector<std::vector<T> >& filteredWaveforms, 
-        std::vector<std::vector<T> >& morphedWaveforms, 
-        std::vector<std::vector<T> >& intrinsicRMS,
-        ArrayBool& selectVals,
-        ArrayBool& roi,
-        std::vector<std::vector<T> >& correctedMedians,
-        const char filterName='d', 
-        const unsigned int grouping=64,
-        const unsigned int structuringElement=5,
-        const unsigned int window=0,
-        const float thresholdFactor=2.5);
-
-
-      template <typename T>
-      void removeCoherentNoise2D(
-        std::vector<std::vector<T> >& waveLessCoherent, 
-        const std::vector<std::vector<T> >& filteredWaveforms,
-        std::vector<std::vector<T> >& morphedWaveforms, 
-        std::vector<std::vector<T> >& intrinsicRMS,
-        ArrayBool& selectVals,
-        ArrayBool& roi,
-        std::vector<std::vector<T> >& correctedMedians,
-        const char filterName='g',
-        const unsigned int grouping=64, 
-        const unsigned int structuringElementx=5,
-        const unsigned int structuringElementy=20,
-        const unsigned int window=0,
-        const float thresholdFactor=2.5);
-    
-  };
+  template <typename T>
+  void removeCoherentNoise2D(
+    std::vector<std::vector<T> >& waveLessCoherent, 
+    const std::vector<std::vector<T> >& filteredWaveforms,
+    std::vector<std::vector<T> >& morphedWaveforms, 
+    std::vector<std::vector<T> >& intrinsicRMS,
+    ArrayBool& selectVals,
+    ArrayBool& roi,
+    std::vector<std::vector<T> >& correctedMedians,
+    const char filterName='g',
+    const unsigned int grouping=64, 
+    const unsigned int structuringElementx=5,
+    const unsigned int structuringElementy=20,
+    const unsigned int window=0,
+    const float thresholdFactor=2.5);
+  
+};
 }
 
 #endif
